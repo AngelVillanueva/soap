348eb5b72dd6c463806c97190b218af155206f3b
Angel Villanueva <avillanueva@sinapse.es>
2014-09-02 09:49:22 +0200

    Move application init to test set up functions

--- administrator/components/com_content/tests/ArticleTest.php
+++ administrator/components/com_content/tests/ArticleTest.php
@@ -1,13 +1,31 @@
 <?php
 class ArticleTest extends \PHPUnit_Framework_TestCase {
          
-    public function testListArticles(){    
-        $input = JFactory::getApplication('administrator')->input; // added after avoiding the unserialized offset error by commenting the line in _bootstrap.php
-
-        $c = new ContentController();      
+    protected function setUp()
+    {
+        $app = JFactory::getApplication('administrator');
+        //initialize session and user
+        $session = JFactory::getSession();
+        $session->set('user', JUser::getInstance('admin'));
+        //bypass JSession::checkToken()
+        JFactory::getApplication()->input->post->set(JSession::getFormToken(),'1');
+    }   
+    public function testListArticles(){   
+        $c = new ContentController();     
         $model = $c->getModel('articles');
         $articles = $model->getItems();
-        var_dump($articles);
+        //var_dump($articles);
+        $this->assertNotEmpty($articles);
+    }
+    public function testSearchArticles(){
+        //simulate user request: searching for 'about'
+        $input = JFactory::getApplication()->input;
+        $input->set('filter_search', 'about');
+          
+        $c = new ContentController();     
+        $model = $c->getModel('articles');             
+        $articles = $model->getItems();
+        //var_dump($articles);
         $this->assertNotEmpty($articles);
     }
 }
