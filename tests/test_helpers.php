aae829b82f57aba66f9907ba3f889e31bc3e29da
Angel Villanueva <avillanueva@sinapse.es>
2014-09-03 18:58:06 +0200

    create tests/test_helpers.php file with shared functions

---
+++ tests/test_helpers.php
@@ -0,0 +1,38 @@
+<?php
+  function crearUsuario($name, $password) {
+    $salt   = JUserHelper::genRandomPassword(32);
+    $crypted  = JUserHelper::getCryptedPassword($password, $salt);
+    $cpassword = $crypted.':'.$salt;
+    $data = array(
+        "name"=> $name,
+        "username"=>'TestAdmin',
+        "password"=>$password,
+        "password2"=>$password,
+        "email"=>'info@example.com',
+        "block"=>0,
+        "groups"=>array("1","2")
+    );
+    $user = new JUser;
+    // Graba el usuario de test en la base de datos
+    if(!$user->bind($data)) {
+        throw new Exception("La estructura de datos no es correcta. Error: " . $user->getError());
+    }
+    if (!$user->save()) {
+        //throw new Exception("Could not save user. Error: " . $user->getError());
+        echo "<br>No pudo grabarse el usuario $name - " . $user->getError();
+    }
+    // Devuelve el id del usuario recién creado
+    return $user->id;
+  }
+  function buscarUsuario($name) {
+    $db = JFactory::getDbo();
+    $query = $db->getQuery(true);
+    $query->select('id');
+    $query->from($db->quoteName('#__users'));
+    $query->where($db->quoteName('name')." = ".$db->quote($name));
+
+    $db->setQuery($query);
+    $result = $db->loadObject();
+
+    return $result;
+  }
