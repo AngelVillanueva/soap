c3a4080ae05ca5bca325d0a35f0048bcb5fbecea
Angel Villanueva <avillanueva@sinapse.es>
2014-09-04 16:54:10 +0200

    Comments

--- plugins/system/autologinurl/autologinurl.php
+++ plugins/system/autologinurl/autologinurl.php
@@ -8,13 +8,13 @@ class plgSystemAutologinurl extends JPlugin {
     $app = JFactory::getApplication('site');
     $jinput = $app->input;
     $credentials = array();
-
+    // determina los nombres de los parámetros en base a la configuración del plugin
     $userparam = $this->params->get('userparam');
     $pwparam = $this->params->get('pwparam');
-
+    // toma los parámetros de la url para utilizarlos en el login
     $credentials['username'] = $jinput->get($userparam);
     $credentials['password'] = $jinput->get($pwparam);
-
+    // si los dos parámetros están presentes intenta logearse
     if (!empty($credentials['username']) && !empty($credentials['password'])) {
       $resultado = $app->login($credentials);
     }
