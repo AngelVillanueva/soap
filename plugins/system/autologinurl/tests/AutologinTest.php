0f927c67c61df2ea1b2ebc20dcef335a92024ab8
Angel Villanueva <avillanueva@sinapse.es>
2014-09-04 16:42:48 +0200

    Test wrong autologin credentials

--- plugins/system/autologinurl/tests/AutologinTest.php
+++ plugins/system/autologinurl/tests/AutologinTest.php
@@ -46,7 +46,6 @@ class AutologinTest extends \PHPUnit_Framework_TestCase {
      * Test para comprobar que el usuario de prueba no se autologea utilizando
      * sus credenciales en la URL de entrada si el username es incorrecto
      * 
-     * @var string $css_element Sólo debe aparecer cuando el usuario está logeado
      * @return boolean True si el test pasa
      */
     public function testAutologinNombreIncorrecto() {
@@ -58,6 +57,34 @@ class AutologinTest extends \PHPUnit_Framework_TestCase {
         $password = datosUsuarioTest()['pw'];
         // cambio el nombre de usuario para que sea incorrecto
         $username = $username."a";
+        // importa el plugin de Autologinurl y recupera el parámetro URL de entrada
+        $plugin = JPluginHelper::getPlugin('system', 'autologinurl');
+        $params = new JRegistry($plugin->params);
+        $aplicacion_web_url = addhttp($params->get('url_aplicacion'));
+        // accede a la aplicación utilizando cURL
+        $curl = curl_init("$aplicacion_web_url?UserId=$username&pwd=$password");
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        $resultado = curl_exec($curl);
+        // TRUE si la página contiene el elemento css que sólo aparece cuando
+        // el usuario está logeado
+        $this->assertNotContains($css_element, $resultado);
+    }
+
+    /**
+     * Test para comprobar que el usuario de prueba no se autologea utilizando
+     * sus credenciales en la URL de entrada si la contraseña es incorrecta
+     * 
+     * @return boolean True si el test pasa
+     */
+    public function testAutologinPasswordIncorrecta() {
+        // inicializa valores
+        // utilizar los mismos valores para $name, $password que en setUp()
+        $app = JFactory::getApplication('site');
+        $css_element = "logout-button";
+        $username = datosUsuarioTest()['username'];
+        $password = datosUsuarioTest()['pw'];
+        // cambio la contraseña para que sea incorrecto
+        $password = $password."ab";
         // importa el plugin de Autologinurl y recupera el parámetro URL de entrada
         $plugin = JPluginHelper::getPlugin('system', 'autologinurl');
         $params = new JRegistry($plugin->params);
